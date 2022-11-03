aec0edd2dbe2360dd48f61448e61ae306c9a2b09
Rootrapee <r.rattanapipit@gmail.com>
2022-11-03 23:46:05 +0700

    add: tests, config to instrument code

---
+++ cypress/e2e/first-test.cy.ts
@@ -0,0 +1,10 @@
+describe("It this Next.js ?", () => {
+  it("should navigate to the about page", () => {
+    // Start from the index page
+    cy.visit("http://localhost:3000/");
+
+    cy.get("h1").contains("Next.js!");
+  });
+});
+
+export {};
--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    swcPlugins: [["swc-plugin-coverage-instrument", "istanbul"]],
+  },
 };
 
 module.exports = nextConfig;
